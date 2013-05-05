d7e88eb8d9fed2f8e48f10511ee59ac5b28cb0a2
Alex Tian <alex.tian@geekpush.com>
2013-05-05 07:17:43 +0000

    min js and css

--- themes/default/head_css.tpl.php
+++ themes/default/head_css.tpl.php
@@ -1,13 +1,18 @@
-<link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/default.css" />
-<link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/system.css" />
-<link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/houstonbbs.css" />
-<link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/html-elements.css" />
-<link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/advanced_forum.css" />
-<link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/advanced_forum-structure.css" />
-<link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/superfish.css" />
-<link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/markitup.style.css" />
-<link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/markitup.bbcode.css" />
-<link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/coin-slider-styles.css" />
-<link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/yp.css" />
-<link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/jquery.rating.css" />
-<link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/privatemsg-view.css" />
+<link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/min_1367737911.css" />
+<?php
+/* DEV
+  <link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/default.css" />
+  <link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/system.css" />
+  <link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/houstonbbs.css" />
+  <link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/html-elements.css" />
+  <link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/advanced_forum.css" />
+  <link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/advanced_forum-structure.css" />
+  <link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/superfish.css" />
+  <link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/markitup.style.css" />
+  <link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/markitup.bbcode.css" />
+  <link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/coin-slider-styles.css" />
+  <link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/yp.css" />
+  <link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/jquery.rating.css" />
+  <link rel="stylesheet" media="all" href="/themes/<?php echo $tpl_theme; ?>/css/privatemsg-view.css" />
+ */
+?>
