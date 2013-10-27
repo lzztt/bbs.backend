d43f74e90d656b07ceca86164686807b26e08aba
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2013-10-27 16:50:05 +0000

    deleting old directories

--- scripts/rsync.sh
+++
@@ -1,4 +0,0 @@
-# do not sync config.php, static/data
-
-rsync $@ -ave 'ssh -p 8022' controller dataobject lzx languages modules route.php portal.php scripts themes web@houstonbbs.com:www.houstonbbs.com/ --exclude=alexa.tpl.php --exclude=.svn
-rsync $@ -ave 'ssh -p 8022' static/themes web@houstonbbs.com:www.houstonbbs.com/static/ --exclude=.svn
