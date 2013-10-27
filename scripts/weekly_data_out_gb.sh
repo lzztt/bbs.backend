13b59ad0c8bf8450edb3496c6571ada0cead593e
Alex Tian <alex.tian@geekpush.com>
2013-10-27 16:50:05 +0000

    deleting old directories

--- scripts/weekly_data_out_gb.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-days=$(ls -lt /home/web/www.houstonbbs.com/logs/access*.gz | wc -l)
-first=$(basename $(ls -lt /home/web/www.houstonbbs.com/logs/access*.gz | tail -n 1 | awk '{print $9}') | cut -c 12-19)
-last=$(basename $(ls -ltr /home/web/www.houstonbbs.com/logs/access*.gz | tail -n 1 | awk '{print $9}') | cut -c 12-19)
-
-echo "$days days ($first - $last)"
-echo -n "data out: "
-zcat /home/web/www.houstonbbs.com/logs/access*.gz | awk '{sum+=$10} END {print sum/1024/1024/1024, "GB"}'
