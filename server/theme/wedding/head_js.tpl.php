37c28803e6263262faf1add180d6ef7a19af3f22
Long <15257546+lzztt@users.noreply.github.com>
2014-06-17 03:18:21 +0000

    update

--- server/theme/wedding/head_js.tpl.php
+++ server/theme/wedding/head_js.tpl.php
@@ -1,5 +1,5 @@
 <script src="//code.jquery.com/jquery-latest.js"></script>
-<script src="/themes/wedding/js/main.js"></script>
+<script src="/themes/wedding/js/main.01.js"></script>
 
 <!--[if IE]>
 	<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
