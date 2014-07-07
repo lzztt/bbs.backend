814772059da699f1c9a400fb3f8fd49b83edbe25
Alex Tian <alex.tian@geekpush.com>
2014-07-07 04:43:20 +0000

    Controller subclass layout and new Design Pattern: Factory Observer Singleton

---
+++ server/controller/adm/AdmCtrler.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace site\controller\adm;
+
+use site\controller\Adm;
+use lzx\html\Template;
+use site\dbobject\AD as ADObject;
+use site\contoller\adm\AD as AD;
+
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of adm
+ *
+ * @author ikki
+ */
+class AdmCtrler extends Adm
+{
+
+   protected function init()
+   {
+      $this->cache->setStatus( FALSE );
+      
+      if ( $this->request->uid !== self::ADMIN_UID )
+      {
+         $this->request->pageNotFound();
+      }
+      
+      Template::$theme = $this->config->theme['adm'];
+   }
+   
+   public function run()
+   {
+      $action = $this->args[1] ? $this->args[1] : 'user';
+      $this->html->var['content'] = $this->run( $action );
+   }
+   
+   public function ad()
+   {
+      (new AD())->run();
+   }
+
+}
+
+//__END_OF_FILE__
