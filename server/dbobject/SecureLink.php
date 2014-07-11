6e57c178a689424ad1d59efacc610749786249a5
Alex Tian <alex.tian@geekpush.com>
2014-07-11 04:48:02 +0000

    update password controller changes

--- server/dbobject/SecureLink.php
+++ server/dbobject/SecureLink.php
@@ -38,23 +38,6 @@ class SecureLink extends DBObject
       return $this->uri . '?r=' . $this->id . '&u=' . $this->uid . '&c=' . $this->code . '&t=' . $this->time;
    }
 
-   static public function loadFromRequest( $uri, Array $get )
-   {
-      $l = new SecureLink();
-
-      if ( isset( $get['r'] ) && isset( $get['u'] ) && isset( $get['c'] ) && isset( $get['t'] ) )
-      {
-         $l->id = $get['r'];
-         $l->uid = $get['u'];
-         $l->code = $get['c'];
-         $l->time = $get['t'];
-         $l->uri = $uri;
-         $l->load( 'id' );
-      }
-
-      return $l;
-   }
-
 }
 
 //__END_OF_FILE__
