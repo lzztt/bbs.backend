e60aba1d5d338d99b06a829da366c34e1439eb50
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2014-07-18 04:54:38 +0000

    new Cache class and FundImporter class

---
+++ script/FundCategory.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace script;
+
+use lzx\db\DBObject;
+use lzx\db\DB;
+
+/**
+ * Description of FundCategory
+ *
+ * @property $id
+ * @property $name
+ */
+class FundCategory extends DBObject
+{
+
+   public function __construct( $id = null, $properties = '' )
+   {
+      $db = DB::getInstance();
+      $table = 'fund_categories';
+      $fields = [
+         'id' => 'id',
+         'name' => 'name'
+      ];
+      parent::__construct( $db, $table, $fields, $id, $properties );
+   }
+
+}
+
+//__END_OF_FILE__
