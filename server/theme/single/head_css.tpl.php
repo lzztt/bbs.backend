cb55ab96d60efb98ab5e6d6f97a50eed81080af3
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2014-07-22 05:12:53 +0000

    new single site login and attendee controller

--- server/theme/single/head_css.tpl.php
+++ server/theme/single/head_css.tpl.php
@@ -1,1 +1,8 @@
-<link href="/themes/single/css/min_1405995171.css" rel="stylesheet" type="text/css" />
+<?php if ( $min ): ?>
+   <link href="/themes/single/css/min_1405995171.css" rel="stylesheet" type="text/css" />
+<?php else: ?>
+   <link href="/themes/single/css/normalize.css" rel="stylesheet" type="text/css" />
+   <link href="/themes/single/css/font.css" rel="stylesheet" type="text/css" />
+   <link href="/themes/single/css/main.css" rel="stylesheet" type="text/css" />
+<?php endif; ?>
+
