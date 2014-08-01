2aefb4727ea115c63cff38d9f2f3c85ca680fd31
Alex Tian <alex.tian@geekpush.com>
2014-08-01 05:28:45 +0000

    add checkin page for single

--- server/controller/single/CheckinCtrler.php
+++ server/controller/single/CheckinCtrler.php
@@ -21,19 +21,24 @@ class CheckinCtrler extends Single
       // logged in    
       $act = \array_pop( $this->db->query( 'CALL get_latest_single_activity()' ) );
       $atd = new FFAttendee();
-      $atd->aid = (int) $act['id'];
+      $atd->aid = (int) $act[ 'id' ];
       $atd->status = 1;
-      
-      $attendeeGroups = [
-         0 => [ ],
-         1 => [ ] ];
 
-      foreach ( $atd->getList('id,name,sex,email,checkin') as $attendee )
+      $confirmed_groups = [[], [ ] ];
+      $atd->where( 'checkin', 0, '>' );
+      $atd->order( 'checkin' );
+      foreach ( $atd->getList( 'name,sex' ) as $attendee )
       {
-         $attendeeGroups[ (int) $attendee[ 'sex' ] ][] = $attendee;
+         $confirmed_groups[ (int) $attendee[ 'sex' ] ][] = $attendee;
       }
 
-      $this->html->var[ 'content' ] = new Template( 'checkin', [ 'groups' => $attendeeGroups ] );
+      $atd->where( 'checkin', 0, '=' );
+      foreach ( $atd->getList( 'id,name,sex' ) as $attendee )
+      {
+         $unconfirmed_groups[ (int) $attendee[ 'sex' ] ][] = $attendee;
+      }
+
+      $this->html->var[ 'content' ] = new Template( 'checkin', ['confirmed_groups' => $confirmed_groups, 'unconfirmed_groups' => $unconfirmed_groups ] );
    }
 
 }
