26f4aed315b1eb2b8848311bc184842f5452570a
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2014-08-01 05:28:45 +0000

    add checkin page for single

--- server/controller/single/AttendeeCtrler.php
+++ server/controller/single/AttendeeCtrler.php
@@ -5,6 +5,7 @@ namespace site\controller\single;
 use site\controller\Single;
 use lzx\html\Template;
 use site\dbobject\FFAttendee;
+
 /**
  * @property \lzx\db\DB $db database object
  */
@@ -14,22 +15,36 @@ class AttendeeCtrler extends Single
    // private attendee info
    public function run()
    {
-      // login first
-      if ( !$this->session->loginStatus )
+      $uid = (int) $this->request->get[ 'u' ];
+      // not a user request
+      if ( $uid == 0 )
       {
-         $this->_displayLogin();
-         return;
+         // login first
+         if ( !$this->session->loginStatus )
+         {
+            $this->_displayLogin();
+            return;
+         }
+      }
+      else
+      {
+         //verify user's access code
+         $code = $this->request->get[ 'c' ];
+         if ( $code != $this->_getCode( $uid ) )
+         {
+            $this->pageForbidden();
+         }
       }
 
-      // logged in    
+      // logged in or from a valid user link 
       if ( TRUE )//$this->request->timestamp < strtotime( "09/16/2013 22:00:00 CDT" ) )
       {
          $a = \array_pop( $this->db->query( 'CALL get_latest_single_activity()' ) );
 
-         $attendee= new FFAttendee();
-         $attendee->aid= $a['id'];
+         $attendee = new FFAttendee();
+         $attendee->aid = $a[ 'id' ];
          $content = [
-            'attendees' => $attendee->getList('name,sex,email,info,time')
+            'attendees' => $attendee->getList( 'name,sex,email,info,time' )
          ];
 
          $this->html->var[ 'content' ] = new Template( 'attendees', $content );
--- server/theme/single/checkin.tpl.php
+++ server/theme/single/checkin.tpl.php
@@ -1,10 +1,15 @@
 <div style="background-color: pink;">
-   <?php foreach ( $groups as $i => $attendees ): ?>
-      <div class="group"><h3><?php print $i ? '男生' : '女生'  ?></h3>
+   <?php foreach ( $confirmed_groups as $i => $attendees ): ?>
+      <div id="confirmed_group_<?php print $i; ?>"><h3>已登记<?php print $i ? '男生' : '女生'  ?></h3>
          <?php foreach ( $attendees as $i => $a ): ?>
-            <div id="attendee_<?php print $a[ 'id' ]; ?>" class="attendee<?php print $a[ 'checkin' ] ? ' confirmed' : ''; ?>">
-               <span><?php print $i + 1; ?></span><?php print $a[ 'name' ]; ?>
-            </div>
+            <div class="attendee confirmed"><span><?php print $i + 1; ?></span><?php print $a[ 'name' ]; ?></div>
+         <?php endforeach; ?>
+      </div>
+   <?php endforeach; ?>
+   <?php foreach ( $unconfirmed_groups as $i => $attendees ): ?>
+      <div id="unconfirmed_group_<?php print $i; ?>"><h3>未登记<?php print $i ? '男生' : '女生'  ?></h3>
+         <?php foreach ( $attendees as $i => $a ): ?>
+            <div id="attendee_<?php print $a[ 'id' ]; ?>" class="attendee unconfirmed"><?php print $a[ 'name' ]; ?></div>
          <?php endforeach; ?>
       </div>
    <?php endforeach; ?>
