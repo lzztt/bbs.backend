30be0d4017a76a31f9974eb7ba254111b08120e1
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2014-08-02 05:32:50 +0000

    working version of single site

--- server/theme/single/head_css.tpl.php
+++ server/theme/single/head_css.tpl.php
@@ -3,6 +3,6 @@
    <link href="/themes/single/css/font.css" rel="stylesheet" type="text/css" />
    <link href="/themes/single/css/main.css" rel="stylesheet" type="text/css" />
 <?php else: ?>
-   <link href="/themes/single/css/min_1405995171.css" rel="stylesheet" type="text/css" />
+   <link href="/themes/single/css/min_1406956143.css" rel="stylesheet" type="text/css" />
 <?php endif; ?>
 
