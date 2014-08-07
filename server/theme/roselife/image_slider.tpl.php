eb1dc0203ed03169ef8fe0f05cfaf2a6766954ab
Alex Tian <alex.tian@geekpush.com>
2014-08-07 05:42:44 +0000

    working navbar now

--- server/theme/roselife/image_slider.tpl.php
+++ server/theme/roselife/image_slider.tpl.php
@@ -1,8 +1,7 @@
-<div id="image_slider">
-   <a id='image' href='#'></a>
+<?php if ( $images ): ?>
    <ul style="display: none;">
       <?php foreach ( $images as $i ): ?>
          <li data-href="/node/<?php print $i[ 'nid' ]; ?>" data-img="<?php print $i[ 'path' ]; ?>"><?php print $i[ 'name' ] . ' @ ' . $i[ 'title' ]; ?></li>
       <?php endforeach; ?>
    </ul>
-</div>
+<?php endif; ?>
