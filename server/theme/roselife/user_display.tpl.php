f7a7f19de1bc78c7f62732f2adb43b22fe3c9a52
Alex Tian <alex.tian@geekpush.com>
2014-08-11 04:53:53 +0000

    more responsive tables

---
+++ server/theme/roselife/user_display.tpl.php
@@ -0,0 +1,38 @@
+<?php print $userLinks; ?>
+<div>
+   <div>
+      <img class="avatar" src="/data/avatars/1-100.png" alt="HoustonBBS的头像">
+      <?php print $username; ?><br />
+      <?php if ( $pm ): ?>
+         <a class="button" href="<?php print $pm; ?>">发送站内短信</a>
+      <?php endif; ?>
+   </div>
+
+   <dl>
+      <?php foreach ( $info as $k => $v ): ?>
+         <dt><?php print $k; ?></dt><dd><?php print $v; ?></dd>
+      <?php endforeach; ?>
+   </dl>
+</div>
+<table class='user_topics'>
+   <caption>最近发表的论坛话题</caption>
+   <thead><tr><th>论坛话题</th><th>发表时间</th></tr></thead>
+   <tbody class="even_odd_parent">
+      <?php foreach ( $topics as $t ): ?>
+         <tr>
+            <td><a href="/node/<?php print $t[ 'nid' ]; ?>"><?php print $t[ 'title' ]; ?></a></td><td><?php print \date( 'm/d/Y H:i', $t[ 'create_time' ] ); ?></td>
+         </tr>
+      <?php endforeach; ?>
+   </tbody>
+</table>
+<table class='user_topics'>
+   <caption>最近回复的论坛话题</caption>
+   <thead><tr><th>论坛话题</th><th>发表时间</th></tr></thead>
+   <tbody class="even_odd_parent">
+      <?php foreach ( $comments as $c ): ?>
+         <tr>
+            <td><a href="/node/<?php print $c[ 'nid' ]; ?>"><?php print $c[ 'title' ]; ?></a></td><td><?php print \date( 'm/d/Y H:i', $c[ 'create_time' ] ); ?></td>
+         </tr>
+      <?php endforeach; ?>
+   </tbody>
+</table>
