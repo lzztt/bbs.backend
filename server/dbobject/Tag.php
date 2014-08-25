4c6f8341028bd28a9c78e1c714ae90ca8b182af8
Alex Tian <alex.tian@geekpush.com>
2014-08-25 05:39:46 +0000

    initial version of muti-site support

--- server/dbobject/Tag.php
+++ server/dbobject/Tag.php
@@ -7,7 +7,6 @@
 namespace site\dbobject;
 
 use lzx\db\DBObject;
-use lzx\core\Cache;
 use lzx\db\DB;
 
 /**
@@ -22,164 +21,161 @@ use lzx\db\DB;
 class Tag extends DBObject
 {
 
-    const FORUM_ID = 1;
-    const YP_ID = 2;
+   public function __construct( $id = null, $properties = '' )
+   {
+      $db = DB::getInstance();
+      $table = 'tags';
+      $fields = [
+         'id' => 'id',
+         'name' => 'name',
+         'description' => 'description',
+         'parent' => 'parent',
+         'root' => 'root',
+         'weight' => 'weight'
+      ];
+      parent::__construct( $db, $table, $fields, $id, $properties );
+   }
 
-    public function __construct( $id = null, $properties = '' )
-    {
-        $db = DB::getInstance();
-        $table = 'tags';
-        $fields = [
-            'id' => 'id',
-            'name' => 'name',
-            'description' => 'description',
-            'parent' => 'parent',
-            'root' => 'root',
-            'weight' => 'weight'
-        ];
-        parent::__construct( $db, $table, $fields, $id, $properties );
-    }
+   /*
+    * rarely used, only get leaves for a root id (forum / yellow page)
+    */
 
-    /*
-     * rarely used, only get leaves for a root id (forum / yellow page)
-     */
-
-    public function getLeafTIDs()
-    {
-        if ( $this->id )
-        {
-            $ids = [];
-            $tree = $this->getTagTree();
-            foreach ( $tree as $i => $t )
+   public function getLeafTIDs()
+   {
+      if ( $this->id )
+      {
+         $ids = [ ];
+         $tree = $this->getTagTree();
+         foreach ( $tree as $i => $t )
+         {
+            if ( !\array_key_exists( 'children', $t ) )
             {
-                if ( !\array_key_exists( 'children', $t ) )
-                {
-                    $ids[] = $i;
-                }
+               $ids[] = $i;
             }
-            return $ids;
-        }
-        return [];
-    }
+         }
+         return $ids;
+      }
+      return [ ];
+   }
 
-    /*
-     * get the tag tree, upto 2 levels
-     */
+   /*
+    * get the tag tree, upto 2 levels
+    */
 
-    public function getTagRoot()
-    {
-        static $root = [];
+   public function getTagRoot()
+   {
+      static $root = [ ];
 
-        if ( isset( $this->id ) )
-        {
-            if ( !\array_key_exists( $this->id, $root ) )
+      if ( isset( $this->id ) )
+      {
+         if ( !\array_key_exists( $this->id, $root ) )
+         {
+            $arr = \array_reverse( $this->call( 'get_tag_root(' . $this->id . ')' ) );
+
+            $tags = [ ];
+            foreach ( $arr as $r )
             {
-                $arr = \array_reverse( $this->call( 'get_tag_root(' . $this->id . ')' ) );
-
-                $tags = [];
-                foreach ( $arr as $r )
-                {
-                    $id = \intval( $r['id'] );
-                    $parent = \is_null( $r['parent'] ) ? NULL : \intval( $r['parent'] );
-                    $tags[$id] = [
-                        'id' => $id,
-                        'name' => $r['name'],
-                        'description' => $r['description'],
-                        'parent' => $parent
-                    ];
-                }
-                $root[$this->id] = $tags;
+               $id = (int) $r[ 'id' ];
+               $parent = \is_null( $r[ 'parent' ] ) ? NULL : (int) $r[ 'parent' ];
+               $tags[ $id ] = [
+                  'id' => $id,
+                  'name' => $r[ 'name' ],
+                  'description' => $r[ 'description' ],
+                  'parent' => $parent
+               ];
             }
-            return $root[$this->id];
-        }
-        else
-        {
-            throw new \Exception( 'no tag id set' );
-        }
-    }
+            $root[ $this->id ] = $tags;
+         }
+         return $root[ $this->id ];
+      }
+      else
+      {
+         throw new \Exception( 'no tag id set' );
+      }
+   }
 
-    public function getTagTree()
-    {
-        static $tree = [];
+   public function getTagTree()
+   {
+      static $tree = [ ];
 
-        if ( isset( $this->id ) )
-        {
-            if ( !\array_key_exists( $this->id, $tree ) )
+      if ( isset( $this->id ) )
+      {
+         if ( !\array_key_exists( $this->id, $tree ) )
+         {
+            $arr = $this->call( 'get_tag_tree(' . $this->id . ')' );
+
+            $tags = [ ];
+            $children = [ ];
+            foreach ( $arr as $r )
             {
-                $arr = $this->call( 'get_tag_tree(' . $this->id . ')' );
+               $id = (int) $r[ 'id' ];
+               $parent = \is_null( $r[ 'parent' ] ) ? NULL : (int) $r[ 'parent' ];
 
-                $tags = [];
-                $children = [];
-                foreach ( $arr as $r )
-                {
-                    $id = \intval( $r['id'] );
-                    $parent = \is_null( $r['parent'] ) ? NULL : \intval( $r['parent'] );
-
-                    $tags[$id] = [
-                        'id' => $id,
-                        'name' => $r['name'],
-                        'description' => $r['description'],
-                        'parent' => $parent,
-                    ];
-                    if ( !\is_null( $parent ) )
-                    {
-                        $children[$parent][] = $id;
-                    }
-                }
-
-                foreach ( $children as $p => $c )
-                {
-                    $tags[$p]['children'] = $c;
-                }
-
-                $tree[$this->id] = $tags;
+               $tags[ $id ] = [
+                  'id' => $id,
+                  'name' => $r[ 'name' ],
+                  'description' => $r[ 'description' ],
+                  'parent' => $parent,
+               ];
+               if ( !\is_null( $parent ) )
+               {
+                  $children[ $parent ][] = $id;
+               }
             }
 
-            return $tree[$this->id];
-        }
-        else
-        {
-            throw new \Exception( 'no tag id set' );
-        }
-    }
+            foreach ( $children as $p => $c )
+            {
+               $tags[ $p ][ 'children' ] = $c;
+            }
 
-    /*
-     * get parent tag
-     */
+            $tree[ $this->id ] = $tags;
+         }
 
-    public function getParent( $properties = '' )
-    {
-        $this->load( 'parent' );
-        if ( \is_null( $this->parent ) )
-        {
-            return NULL;
-        }
-        else
-        {
-            $tag = new Tag();
-            $tag->id = $this->parent;
-            $parent = $tag->getList( $properties, 1 );
-            return \array_pop( $parent );
-        }
-    }
+         return $tree[ $this->id ];
+      }
+      else
+      {
+         throw new \Exception( 'no tag id set' );
+      }
+   }
 
-    /*
-     * get children tags
-     */
+   /*
+    * get parent tag
+    */
 
-    public function getChildren( $properties = '' )
-    {
-        $tag = new Tag();
-        $tag->parent = $this->id;
-        $tag->order( 'weight' );
-        return $tag->getList( $properties );
-    }
+   public function getParent( $properties = '' )
+   {
+      $this->load( 'parent' );
+      if ( \is_null( $this->parent ) )
+      {
+         return NULL;
+      }
+      else
+      {
+         $tag = new Tag();
+         $tag->id = $this->parent;
+         $parent = $tag->getList( $properties, 1 );
+         return \array_pop( $parent );
+      }
+   }
 
-    // get the information for the latest updated node
-    public function getNodeInfo()
-    {
-        return $this->call( 'get_tag_node_info_1(' . $this->id . ')' );
-    }
+   /*
+    * get children tags
+    */
+
+   public function getChildren( $properties = '' )
+   {
+      $tag = new Tag();
+      $tag->parent = $this->id;
+      $tag->order( 'weight' );
+      return $tag->getList( $properties );
+   }
+
+   // get the information for the latest updated node
+   public function getNodeInfo()
+   {
+      return $this->call( 'get_tag_node_info_1(' . $this->id . ')' );
+   }
 
 }
 
---
+++ server/theme/roselife/html.dallas.tpl.php
@@ -0,0 +1,106 @@
+<!DOCTYPE html>
+<html lang='zh' dir='ltr'>
+   <head>
+      <meta charset='UTF-8'>
+      <meta name='description' content='<?php print $head_description; ?>'>      
+      <meta name='viewport' content='width=device-width, initial-scale=1'>
+
+      <!--BEBIN JS-->
+      <!--[if lt IE 9]>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.2/html5shiv.min.js"></script>
+      <script src="//cdnjs.cloudflare.com/ajax/libs/respond.js/1.4.2/respond.min.js"></script>
+      <![endif]-->
+      <script>
+         if ('querySelector' in document && 'localStorage' in window && 'addEventListener' in window) {
+            document.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"><\/script>');
+         } else {
+            document.write('<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"><\/script>');
+         }
+      </script>
+      <script>
+         if (!window.jQuery)
+         {
+            if ('querySelector' in document && 'localStorage' in window && 'addEventListener' in window) {
+               document.write('<script src="//ajax.aspnetcdn.com/ajax/jQuery/jquery-2.1.1.min.js"><\/script>');
+            }
+            else {
+               document.write('<script src="//ajax.aspnetcdn.com/ajax/jQuery/jquery-1.11.1.min.js"><\/script>');
+            }
+         }
+      </script>
+
+      <script>(typeof JSON === 'object') || document.write('<script src="//cdnjs.cloudflare.com/ajax/libs/json3/3.3.2/json3.min.js"><\/script>')</script>
+
+      <?php if ( $tpl_debug ): ?>
+         <script src="/themes/<?php print $tpl_theme; ?>/js/jquery.cookie.js"></script>
+         <script src="/themes/<?php print $tpl_theme; ?>/js/jquery.imageslider.js"></script>
+         <script src="/themes/<?php print $tpl_theme; ?>/js/jquery.hoverIntent.js"></script>
+         <script src="/themes/<?php print $tpl_theme; ?>/js/jquery.superfish.js"></script>
+         <script src="/themes/<?php print $tpl_theme; ?>/js/jquery.markitup.js"></script>
+         <script src="/themes/<?php print $tpl_theme; ?>/js/jquery.markitup.bbcode.set.js"></script>
+         <script src="/themes/<?php print $tpl_theme; ?>/js/jquery.upload-1.0.2.js"></script>
+         <script src="/themes/<?php print $tpl_theme; ?>/js/main.js"></script>
+         <script src="/themes/<?php print $tpl_theme; ?>/js/main.dallas.js"></script>
+      <?php else: ?>
+         <script src="/themes/<?php print $tpl_theme; ?>/js/min_1408898412.js"></script>
+         <script src="/themes/<?php print $tpl_theme; ?>/js/min_1408898412.dallas.js"></script>
+      <?php endif; ?>
+      <!--END JS-->
+
+      <!--BEGIN CSS-->
+      <?php if ( $tpl_debug ): ?>
+         <link href="/themes/<?php print $tpl_theme; ?>/css/normalize.css" rel="stylesheet" type="text/css">
+         <link href="/themes/<?php print $tpl_theme; ?>/css/markitup.style.css" rel="stylesheet" type="text/css">
+         <link href="/themes/<?php print $tpl_theme; ?>/css/markitup.bbcode.css" rel="stylesheet" type="text/css">
+         <link href="/themes/<?php print $tpl_theme; ?>/css/nav_xs.css" rel="stylesheet" type="text/css">
+         <link href="/themes/<?php print $tpl_theme; ?>/css/nav_sm.css" rel="stylesheet" type="text/css">
+         <link href="/themes/<?php print $tpl_theme; ?>/css/main_xs.css" rel="stylesheet" type="text/css">
+         <link href="/themes/<?php print $tpl_theme; ?>/css/main_sm.css" rel="stylesheet" type="text/css">
+         <link href="/themes/<?php print $tpl_theme; ?>/css/main_md.css" rel="stylesheet" type="text/css">
+         <link href="/themes/<?php print $tpl_theme; ?>/css/main_lg.css" rel="stylesheet" type="text/css">
+         <link href="/themes/<?php print $tpl_theme; ?>/css/main.dallas.css" rel="stylesheet" type="text/css">
+      <?php else: ?>
+         <link href="/themes/<?php print $tpl_theme; ?>/css/min_1408898412.css" rel="stylesheet" type="text/css">
+         <link href="/themes/<?php print $tpl_theme; ?>/css/min_1408898412.dallas.css" rel="stylesheet" type="text/css">
+      <?php endif; ?>
+      <!--END CSS-->
+
+      <title><?php print $head_title; ?></title>
+      <!--BEING APPLE ICON-->
+      <link rel='apple-touch-icon' href='/apple-touch-icon.png'>
+      <link rel='apple-touch-icon' sizes='72x72' href='/apple-touch-icon-72x72.png'>
+      <link rel='apple-touch-icon' sizes='114x114' href='/apple-touch-icon-114x114.png'>
+      <!--END APPLE ICON-->
+   </head>
+   <body>
+      <div id='page'>
+         <header id='page_header'>
+            <div id="logo_div">
+               <a id='logo' href='/'><img src='/themes/roselife/images/logo.png'></a>
+               <span id='messagebox'><?php print $head_title; ?></span>
+            </div>
+         </header>
+         <nav id='page_navbar' class='hidden'>
+            <ul class="sf-menu" style="display: inline-block; float: right;">
+               <li class='v_user'><a><?php print $username; ?></a></li>
+               <li class='v_user'><a id="pm" href="/pm/mailbox">短信</a></li>
+               <li class='v_user'><a href="/user/bookmark">收藏夹</a></li>
+               <li class='v_user'><a href="/user">我的账户</a></li>
+               <li class='v_user'><a href="/password/change">更改密码</a></li>
+               <li class='v_user'><a  href="/user/logout">登出</a></li>
+               <li class='v_guest'><a href="/user/login">登录</a></li>
+               <li class='v_guest'><a href="/password/forget">忘记密码</a></li>
+               <li class='v_guest'><a href="/user/username">忘记用户名</a></li>
+               <li class='v_guest'><a href="/user/register">注册帐号</a></li>
+            </ul>
+            <?php print $page_navbar; ?>
+         </nav>
+         <section id='page_body'><?php print $content; ?></section>
+         <footer id='page_footer'>
+            <div id='copyright'>如有问题，请联络网站管理员<a href="mailto:admin@dallasbbs.com">admin@dallasbbs.com</a> | © 2009-2014 HoustonBBS 版权所有 | <a href='/term'>免责声明</a></div>
+         </footer>
+      </div>
+   </body>
+   <?php if ( !$debug ): ?>
+   <?php endif; ?>
+</html>
