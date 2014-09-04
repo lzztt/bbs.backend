fa1dbd7104d632b7c65330eadb55183c38f9da9b
Alex Tian <alex.tian@geekpush.com>
2014-09-04 04:55:06 +0000

    alexa tpl

--- script/CronApp.php
+++ script/CronApp.php
@@ -209,10 +209,18 @@ class CronApp extends App
       }
    }
 
-// daily at 23:55 CDT
+   // daily at 23:55 CDT
    protected function do_alexa()
    {
-      $c = \curl_init( 'http://data.alexa.com/data?cli=10&dat=s&url=http://www.houstonbbs.com' );
+      foreach ( ['houston', 'dallas' ] as $city )
+      {
+         $this->_updateAlexa( $city );
+      }
+   }
+
+   private function _updateAlexa( $city )
+   {
+      $c = \curl_init( 'http://data.alexa.com/data?cli=10&dat=s&url=http://www.' . $city . 'bbs.com' );
       \curl_setopt_array( $c, [
          CURLOPT_RETURNTRANSFER => TRUE,
          CURLOPT_CONNECTTIMEOUT => 2,
@@ -227,8 +235,8 @@ class CronApp extends App
          if ( $p[ 2 ] )
          {
             $rank = \number_format( intval( $p[ 2 ] ) );
-            $data = 'HoustonBBS最近三个月平均访问量<a href="http://www.alexa.com/data/details/main?url=http://www.houstonbbs.com" title="HoustonBBS近三个月的访问量统计">Alexa排名</a>:<br /><a href="/node/5641" title="Houston各中文网站 月访问量 横向比较">第 <b>' . $rank . '</b> 位</a> (更新时间: ' . \date( 'm/d/Y H:i:s T', \intval( $_SERVER[ 'REQUEST_TIME' ] ) ) . ')';
-            \file_put_contents( $this->config->path[ 'theme' ] . '/' . $this->config->theme[ 'roselife' ] . '/alexa.tpl.php', $data );
+            $data = \ucfirst( $city ) . 'BBS最近三个月平均访问量<a href="http://www.alexa.com/data/details/main?url=http://www.' . $city . 'bbs.com">Alexa排名</a>:<br /><a href="http://www.alexa.com/data/details/main?url=http://www.' . $city . 'bbs.com">第 <b>' . $rank . '</b> 位</a> (更新时间: ' . \date( 'm/d/Y H:i:s T', \intval( $_SERVER[ 'REQUEST_TIME' ] ) ) . ')';
+            \file_put_contents( $this->config->path[ 'theme' ] . '/' . $this->config->theme[ 'roselife' ] . '/alexa.' . $city . '.tpl.php', $data );
          }
          else
          {
