345f575fe808b510de0a753e448936f33464d412
Long <15257546+lzztt@users.noreply.github.com>
2014-09-20 05:41:59 +0000

    enable austinbbs

--- server/theme/roselife/home.austin.tpl.php
+++ server/theme/roselife/home.austin.tpl.php
@@ -1,4 +1,7 @@
-<?php print $breadcrumb; ?>
+<div class="image_slider"><?php print $imageSlider; ?></div><section class="items">
+   <header>最新话题</header><?php print $latestForumTopics; ?></section><section class="items">
+   <header>最新论坛回复</header><?php print $latestForumTopicReplies; ?></section><section class="items home_hot_nodes">
+   <header>本周热门</header><?php print $hotForumTopics; ?></section>
 <?php foreach ( $groups as $group_id => $tags ): ?>
    <table>
       <thead>
@@ -19,15 +22,22 @@
       </tbody>
    </table> 
 <?php endforeach; ?>
-
+<div id='site_stat' class='ajax_load' data-ajax='/home/ajax/stat'>
+   <div>当前在线用户：<span class='ajax_onlineCount'></span> (<span class='ajax_onlineUserCount'></span> 用户| <span class='ajax_onlineGuestCount'></span> 访客)</div>
+   <div><span class='ajax_onlineUsers'></span></div>
+   <div>统计</div>
+   <div>
+      <section>
+         <span class='ajax_nodeCount'></span> 主题，<span class='ajax_postCount'></span> 贴子，<span class='ajax_userCount'></span> 用户，欢迎新进会员 <span class='ajax_latestUser'></span>
+         <br>
+         今日新主题 <span class='ajax_nodeTodayCount'></span> 个，今日新评论 <span class='ajax_commentTodayCount'></span> 个，今日新用户 <span class='ajax_userTodayCount'></span> 个
+      </section><section><span class='ajax_alexa'></span></section>
+   </div>
+</div>
 <div style="border:1px solid #DDDDDD; margin-top:15px; clear: both;">
    <div style="padding:0.4em 0.7em;">友情链接</div>
    <div style="padding:0.4em 0.7em; background:#EEEEEE none repeat scroll 0 0;">
+      <a style="padding: 4px;" target="_blank" href="http://www.houstonbbs.com">缤纷休斯顿</a>
       <a style="padding: 4px;" target="_blank" href="http://www.dallasbbs.com">缤纷达拉斯</a>
-      <a style="padding: 4px;" target="_blank" href="http://www.austinbbs.com">缤纷奥斯汀</a>
-      <a style="padding: 4px;" target="_blank" href="http://www.utcssa.net">UT Austin CSSA</a>
-      <a style="padding: 4px;" target="_blank" href="http://www.uslifes.com">海外生活</a>
-      <a style="padding: 4px;" target="_blank" href="http://www.xibian.net">西边摄影</a>
-      <a style="padding: 4px;" target="_blank" href="http://www.hi8d.com">HI8D国际短信</a>
    </div>
 </div>
