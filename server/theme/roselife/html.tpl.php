3a6e87e9852282959f7e97bf178273698bfde55f
Long <15257546+lzztt@users.noreply.github.com>
2014-10-19 22:40:17 +0000

    update popup css and js

--- server/theme/roselife/html.tpl.php
+++ server/theme/roselife/html.tpl.php
@@ -28,7 +28,7 @@
          <script defer src="/themes/<?php print $tpl_theme; ?>/js/jquery.upload-1.0.2.js"></script>
          <script defer src="/themes/<?php print $tpl_theme; ?>/js/main.js"></script>
       <?php else: ?>
-         <script defer src="/themes/<?php print $tpl_theme; ?>/js/min_1413653636.js"></script>
+         <script defer src="/themes/<?php print $tpl_theme; ?>/js/min_1413757230.js"></script>
       <?php endif; ?>
       <!--END JS-->
 
@@ -45,7 +45,7 @@
          <link href="/themes/<?php print $tpl_theme; ?>/css/main_lg.css" rel="stylesheet" type="text/css">
          <link href="/themes/<?php print $tpl_theme; ?>/css/fontello.css" rel="stylesheet" type="text/css">
       <?php else: ?>
-         <link href="/themes/<?php print $tpl_theme; ?>/css/min_1413653636.css" rel="stylesheet" type="text/css">
+         <link href="/themes/<?php print $tpl_theme; ?>/css/min_1413757230.css" rel="stylesheet" type="text/css">
       <?php endif; ?>
       <!--END CSS-->
 
