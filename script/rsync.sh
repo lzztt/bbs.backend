2dbf049eac9b96b9433da1efb4b4732268dbe987
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2014-11-26 01:08:03 +0000

    updated user app

--- script/rsync.sh
+++ script/rsync.sh
@@ -1,6 +1,7 @@
 # do not sync config.php, static/data
 # rsync -ave 'ssh -p 3355' client/fonts web@houstonbbs.com:bbs/client/
 rsync -ave 'ssh -p 3355' client/themes/roselife web@houstonbbs.com:bbs/client/themes/
+rsync -ave 'ssh -p 3355' client/app web@houstonbbs.com:bbs/client/
 rsync -ave 'ssh -p 3355' lib/lzx web@houstonbbs.com:bbs/lib/
 rsync -ave 'ssh -p 3355' script web@houstonbbs.com:bbs/
 rsync -ave 'ssh -p 3355' server web@houstonbbs.com:bbs/ --exclude='Config.php'
---
+++ server/controller/app/UserCtrler.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace site\controller\app;
+
+use site\controller\App;
+
+class UserCtrler extends App
+{
+
+   private $name = 'user';
+
+   public function getLatestVersion()
+   {
+      $current = NULL;
+      try
+      {
+         $current = \file_get_contents( $this->config->path[ 'file' ] . '/app/user.current' );
+      }
+      catch ( \Exception $ex )
+      {
+         // ignore and continue
+      }
+
+      if ( $current )
+      {
+         $dir = $this->config->path[ 'file' ] . '/app/' . $current;
+         if ( \is_dir( $dir ) )
+         {
+            return $dir;
+         }
+      }
+
+      // need to search
+      $dirs = \glob( $this->config->path[ 'file' ] . '/app/user.*', \GLOB_ONLYDIR );
+      // not found
+      if ( !$dirs )
+      {
+         $this->pageNotFound();
+      }
+
+      $count = \count( $dirs );
+      $dir = $count == 1 ? $dirs[ 0 ] : $dirs[ $count - 2 ];
+
+      // cache the latest version
+      try
+      {
+         \file_put_contents( $this->config->path[ 'file' ] . '/app/user.current', \basename( $dir ) );
+      }
+      catch ( \Exception $ex )
+      {
+         // ignore and continue
+      }
+
+      return $dir;
+   }
+
+   public function run()
+   {
+      $this->response->setContent( \file_get_contents( $this->getLatestVersion() . '/index.html' ) );
+   }
+
+}
+
+//__END_OF_FILE__
