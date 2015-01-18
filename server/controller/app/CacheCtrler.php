1976ce7b883bdd62d35744e2b7ab4bb1c30317cf
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2015-01-18 04:52:26 +0000

    latest image cropper in user app

--- server/controller/app/CacheCtrler.php
+++ server/controller/app/CacheCtrler.php
@@ -7,56 +7,11 @@ use site\controller\App;
 class CacheCtrler extends App
 {
 
-   private $name = 'cache';
-
-   public function getLatestVersion()
-   {
-      $current = NULL;
-      try
-      {
-         $current = \file_get_contents( $this->config->path[ 'file' ] . '/app/cache.current' );
-      }
-      catch ( \Exception $ex )
-      {
-         // ignore and continue
-      }
-
-      if ( $current )
-      {
-         $dir = $this->config->path[ 'file' ] . '/app/' . $current;
-         if ( \is_dir( $dir ) )
-         {
-            return $dir;
-         }
-      }
-
-      // need to search
-      $dirs = \glob( $this->config->path[ 'file' ] . '/app/cache.*', \GLOB_ONLYDIR );
-      // not found
-      if ( !$dirs )
-      {
-         $this->pageNotFound();
-      }
-
-      $count = \count( $dirs );
-      $dir = $count == 1 ? $dirs[ 0 ] : $dirs[ $count - 2 ];
-
-      // cache the latest version
-      try
-      {
-         \file_put_contents( $this->config->path[ 'file' ] . '/app/cache.current', \basename( $dir ) );
-      }
-      catch ( \Exception $ex )
-      {
-         // ignore and continue
-      }
-
-      return $dir;
-   }
+   private $_name = 'cache';
 
    public function run()
    {
-      $this->response->setContent( \file_get_contents( $this->getLatestVersion() . '/index.html' ) );
+      $this->response->setContent( \file_get_contents( $this->_getLatestVersion( $this->_name ) . '/index.html' ) );
    }
 
 }
