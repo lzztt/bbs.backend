dbfe296eab2a4071618a12a138ae04be7c11e11d
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2015-07-04 00:18:10 -0500

    refined single activity site

--- server/theme/single/head_css.tpl.php
+++ server/theme/single/head_css.tpl.php
@@ -2,6 +2,5 @@
    <link href="/themes/single/css/normalize.css" rel="stylesheet" type="text/css" />
    <link href="/themes/single/css/main.css" rel="stylesheet" type="text/css" />
 <?php else: ?>
-   <link href="/themes/single/css/min_1435962820.css" rel="stylesheet" type="text/css" />
+   <link href="/themes/single/css/min_1435986716.css" rel="stylesheet" type="text/css" />
 <?php endif; ?>
-
