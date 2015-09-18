a2cc2965120a9c03862ed892bf0b1865fd69e987
Alex Tian <alex.tian@geekpush.com>
2015-09-18 18:40:19 -0500

    update yp join page

--- server/theme/roselife/yp_join.tpl.php
+++ server/theme/roselife/yp_join.tpl.php
@@ -1,13 +1,37 @@
    <div class="content_header">
       <div class="breadcrumb"><a title="" href="/yp">黄页</a></div>
-      <h3>加入 缤纷休斯顿 电子黄页</h3>
-      <div>
-         请发送下列必要信息到 <a href='mailto:admin@houstonbbs.com'>admin@houstonbbs.com</a><br>
-         信息通过管理员审核后会被添加到<a href="/yp">缤纷休斯顿 电子黄页</a>系统<br>
-         <br>
-         <b>黄页分类</b>选项请参照<a href="/yp">黄页首页</a>
-         <br>
-         <br>
-         <img alt="黄页信息" src="/themes/default/images/yp_edit.png">
-      </div>
+    <article style="text-align: center">
+      <h3>市场推广 先入为主</h3>
+      <h4>带您进入社交网络的市场圈地时代</h4>
+         <div class="article_content">
+           <div class="attach_images">
+             <figure>
+               <figcaption>老客户在这里推荐您，新客户在这里与您互动，未来客户在这里关注您</figcaption>
+               <img src="/data/ad/yp_ad.jpg">
+             </figure>
+             <br><br>
+             <figure>
+               <figcaption>1. 手机电脑随时访问，直达消费者视线</figcaption>
+               <img src="/data/ad/yp_01.jpg">
+             </figure>
+             <figure>
+               <figcaption>2. 推广内容随时更新，客户间评论分享透明可见</figcaption>
+               <img src="/data/ad/yp_02.jpg">
+             </figure>
+             <figure>
+               <figcaption>3. 牢固消费者心中的品牌形象，扩大品牌影响力</figcaption>
+               <img src="/data/ad/yp_03.jpg">
+             </figure>
+             <figure>
+               <figcaption>4. 价格便宜，专业市场推广省时省力</figcaption>
+               <img src="/data/ad/yp_04.jpg">
+             </figure>
+           </div>
+         </div>
+      <br>
+      Google Analytics统计显示：缤纷休斯顿网 平均每天有<span style="color:blue; padding:0">2千2百</span>位独立访客访问页面<span style="color:blue; padding:0">3万8千</span>次，每月有<span style="color:blue; padding:0">2万4千</span>位独立访客访问页面<span style="color:blue; padding:0">104万</span>次。<span style="color:blue; padding:0">86%</span>的用户位于休斯顿地区。<br><br>
+      <span style="color:blue; font-size: 120%">感谢您对电子黄页感兴趣！<br>我们给新客户三个月的免费黄页广告！</span><br>
+        既然都看到这里了，那就联系Email <a href='mailto:yp@houstonbbs.com'>yp@houstonbbs.com</a> 开始添加您的黄页吧 :)
+<br><br><br><br>
+    </article>
    </div>
