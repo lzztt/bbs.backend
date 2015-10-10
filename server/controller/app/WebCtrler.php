e94b70458224dc51a956eea57f240b2122a0404e
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2015-10-10 18:06:11 -0500

    update lzx

---
+++ server/controller/app/WebCtrler.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace site\controller\app;
+
+use site\controller\App;
+
+class WebCtrler extends App
+{
+
+   private $_name = 'web';
+
+   public function run()
+   {
+      $this->response->setContent( \file_get_contents( $this->_getLatestVersion( $this->_name ) . '/index.html' ) );
+   }
+
+}
+
+//__END_OF_FILE__
