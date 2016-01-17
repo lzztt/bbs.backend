e69b1fcd05e6d432c9df01a4efd2523207549efa
Alex Tian <alex.tian@geekpush.com>
2016-01-16 23:58:50 -0600

    fix youtube BBCode

--- server/controller/node/TagCtrler.php
+++ server/controller/node/TagCtrler.php
@@ -44,6 +44,36 @@ class TagCtrler extends Node
          $this->pageForbidden();
       }
    }
+   
+   private function _tagYellowPage( $nid )
+   {
+      if ( empty( $this->args ) )
+      {
+         $this->error( 'no tag id specified' );
+      }
+
+      $newTagID = (int) $this->args[ 0 ];
+
+      $nodeObj = new NodeObject( $nid, 'uid,tid' );
+      if ( $this->request->uid == 1 || $this->request->uid == $nodeObj->uid )
+      {
+         $oldTagID = $nodeObj->tid;
+         $nodeObj->tid = $newTagID;
+         $nodeObj->update( 'tid' );
+
+         foreach ( ['/yp/' . $oldTagID, '/yp/' . $newTagID, '/node/' . $nid ] as $key )
+         {
+            $this->_getIndependentCache( $key )->delete();
+         }
+
+         $this->pageRedirect( '/node/' . $nid );
+      }
+      else
+      {
+         $this->logger->warn( 'wrong action : uid = ' . $this->request->uid );
+         $this->pageForbidden();
+      }
+   }
 
 }
 
