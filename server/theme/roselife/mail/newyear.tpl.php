ebe141fe965f2014a4f4700af9a1c17806464c7e
Long <15257546+lzztt@users.noreply.github.com>
2017-04-29 09:28:42 -0500

    new mailer and ad mails

--- server/theme/roselife/mail/newyear.tpl.php
+++ server/theme/roselife/mail/newyear.tpl.php
@@ -8,11 +8,9 @@
     <article>
       Hi <?php print $username; ?>，<br>
       <br>
-      不知不觉猴年就来到了，今天终于有时间写封邮件拜个年，感谢你<?php print $time; ?>对缤纷<?php print $city; ?>网的支持和关注。<br>
-      祝你和你的家人新年快乐，猴年吉祥！<br>
-      <br>
-      过去一年里，我认识到communication是一项很重要的技能。<br>
-      新的一年里，保持联系！<br>
+      新的一年又到了，感谢您<?php print $time; ?>对缤纷<?php print $city; ?>网的支持和关注。<br><br>
+      祝您和您的家人 新年快乐，鸡年吉祥！<br>
+      在新的一年里 身体健康，心想事成！<br>
       <br>
       站长<br>
       田龙璋
