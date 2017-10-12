7d1e0252d9dd72a15f1f02f54984f73ebaf5523a
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2017-10-11 21:24:12 -0700

    update home page

--- script/backup.sh
+++ script/backup.sh
@@ -1,3 +1,3 @@
-rsync -ave 'ssh -p 3355' web@houstonbbs.com:/home/backup/server_conf*tar.gz /data/backup/houstonbbs/conf/
-rsync -ave 'ssh -p 3355' web@houstonbbs.com:bbs/log/{access,error}_*.log-*.gz /data/backup/houstonbbs/log/
-rsync -ave 'ssh -p 3355' web@houstonbbs.com:bbs/backup/*.sql.gz /data/backup/houstonbbs/db/
+rsync -ave 'ssh' web@houstonbbs.com:/home/backup/server_conf*tar.gz /data/backup/houstonbbs/conf/
+rsync -ave 'ssh' web@houstonbbs.com:bbs/log/{access,error}_*.log-*.gz /data/backup/houstonbbs/log/
+rsync -ave 'ssh' web@houstonbbs.com:bbs/backup/*.sql.gz /data/backup/houstonbbs/db/
