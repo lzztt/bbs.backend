ffaf760ece36f907e4b10e2bac02da079461289e
longztian <ikki3355@gmail.com>
2017-11-09 21:19:12 -0800

    delete old themes

--- server/theme/default/mail/newuser.tpl.php
+++
@@ -1,9 +0,0 @@
-<?= $username ?> 您好，
-
-谢谢您注册 <?= $sitename ?> 网站会员
-
-请点击下面的链接激活您的帐户并且设置用户密码:
-http://www.houstonbbs.com<?= $uri ?>
-
-
-如果遇到问题，请联系网站管理员 admin@houstonbbs.com
--- server/theme/default/user_forgetusername.tpl.php
+++
@@ -1,9 +0,0 @@
-<?= $userLinks ?>
-<form accept-charset="UTF-8" autocomplete="off" method="post" action="/user/username" id="user-pass">
-  <div class="form_element">
-    <label data-help="输入您注册时使用的电子邮箱地址">注册电子邮箱地址</label><input size="22" name="email" type="email" required="required">
-  </div>
-  <div class="form_element">
-    <button type="submit">邮寄您的用户名</button>
-  </div>
-</form>
