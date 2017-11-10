a9454b3cdb66bd76fa1964b3b255cf1d3da5167d
longztian <ikki3355@gmail.com>
2017-11-09 21:19:12 -0800

    delete old themes

--- server/theme/default/image_slider.tpl.php
+++
@@ -1,7 +0,0 @@
-<div id="coin-slider">
-  <?php foreach ($images as $i): ?>
-    <a style="display: none;" href="/node/<?= $i['nid'] ?>">
-      <img src="/img.png" data-src="<?= $i['path'] ?>" alt="<?= $i['name'] . ' @ ' . $i['title'] ?>" />
-    </a>
-  <?php endforeach ?>
-</div>
