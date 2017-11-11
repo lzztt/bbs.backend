38649f161b457ad9f6c10660910d4b16d796a5a9
longztian <ikki3355@gmail.com>
2017-11-11 14:27:05 -0800

    delete obsolete templates and dbobjects

--- server/theme/roselife/user_forgetusername.tpl.php
+++
@@ -1,9 +0,0 @@
-<?= $userLinks ?>
-<form accept-charset="UTF-8" method="post">
-  <fieldset>
-    <label class='label' data-help="输入您注册时使用的电子邮箱地址">注册电子邮箱地址</label><input size="22" name="email" type="email" required autofocus>
-  </fieldset>
-  <fieldset>
-    <button type="submit">发送您的用户名</button>
-  </fieldset>
-</form>
