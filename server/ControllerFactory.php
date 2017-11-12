125b9a0c197434a3f6fac04d85020de001e61bd0
longztian <ikki3355@gmail.com>
2017-11-11 23:17:55 -0800

    refactor method

--- server/ControllerFactory.php
+++ server/ControllerFactory.php
@@ -29,6 +29,21 @@ class ControllerFactory
      */
     public static function createController(Request $req, Response $response, Config $config, Logger $logger, Session $session)
     {
+        list($cls, $args) = self::getHandlerClassAndArgs($req);
+
+        if ($cls) {
+            $handler = new $cls($req, $response, $config, $logger, $session);
+            $handler->args = $args;
+            return $handler;
+        } else {
+            // cannot find a controller
+            $response->pageNotFound();
+            throw new \Exception();
+        }
+    }
+    
+    private static function getHandlerClassAndArgs(Request $req)
+    {
         $args = $req->getURIargs($req->uri);
 
         $keys = array_filter($args, function ($value) {
@@ -50,15 +65,7 @@ class ControllerFactory
                 array_pop($keys);
             }
         }
-
-        if ($cls) {
-            $handler = new $cls($req, $response, $config, $logger, $session);
-            $handler->args = array_values(array_diff($args, $keys));
-            return $handler;
-        } else {
-            // cannot find a controller
-            $response->pageNotFound();
-            throw new \Exception();
-        }
+        
+        return [$cls, array_values(array_diff($args, $keys))];
     }
 }
