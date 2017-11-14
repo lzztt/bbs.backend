392054a454f91ef520115c598b3b9e77450d213a
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2017-11-13 20:47:59 -0800

    fix id for comment handler

--- server/handler/comment/delete/Handler.php
+++ server/handler/comment/delete/Handler.php
@@ -6,14 +6,13 @@ use site\handler\comment\Comment;
 use site\dbobject\Tag;
 use site\dbobject\Comment as CommentObject;
 use site\dbobject\Node;
-use site\dbobject\User;
 
 class Handler extends Comment
 {
     public function run()
     {
         $comment = new CommentObject();
-        $comment->id = $this->id;
+        $comment->id = (int) $this->args[0];
         $comment->load('uid,nid');
 
         if ($this->request->uid != 1 && $this->request->uid != $comment->uid) {
@@ -31,22 +30,7 @@ class Handler extends Comment
 
         if (in_array($node->tid, ( new Tag(self::$city->YPRootID, null) )->getLeafTIDs())) { // yellow page tag
             $this->getCacheEvent('YellowPageComment', $node->tid)->trigger();
-            /*
-              $c = new CommentObject();
-              $c->nid = $comment->nid;
-              $c->uid = $comment->uid;
-              if ( $c->getCount() == 0 )
-              {
-              $node = new Node();
-              $node->deleteRating( $comment->nid, $comment->uid );
-              } */
         }
-
-        /*
-        $user = new User( $comment->uid, 'points' );
-        $user->points -= 1;
-        $user->update( 'points' );
-         */
 
         $comment->delete();
 
