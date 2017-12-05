299ebc1381b6fb27c2e3037666e9d12cabddba50
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2017-12-04 23:19:56 -0800

    remove single and wedding site

--- server/theme/roselife/page_navbar.tpl.php
+++ server/theme/roselife/page_navbar.tpl.php
@@ -2,7 +2,6 @@
   <li><a href="/">首页</a></li>
   <li><a href="/search">搜索</a></li>
   <li><a href="/activity">活动</a></li>
-  <li><a href="/single">单身交友</a></li>
   <li><a href="/forum">论坛</a>
     <ul style="display: none;">
       <li><a href="/help">论坛帮助</a></li>
--- server/theme/wedding2/checkin.tpl.php
+++
@@ -1,7 +0,0 @@
-<?php foreach ($tables as $i => $guests): ?>
-  <div class="table"><div class="table_name">桌号: <?= $i ?></div>
-  <?php foreach ($guests as $g): ?>
-    <div id="guest_<?= $g['id'] ?>" class="<?= $g['checkin'] ? 'confirmed' : 'guest' ?>"><?= $g['name'] ?> (<?= $g['guests'] ?>)</div>
-  <?php endforeach ?>
-  </div>
-<?php endforeach ?>
