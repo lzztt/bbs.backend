86e06622b2c1d9b1e77331da654df27f6fd94221
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2018-01-20 19:59:38 -0800

    update password hash to argon2i

---
+++ server/handler/unsubscribe/Handler.php
@@ -0,0 +1,21 @@
+<?php declare(strict_types = 1);
+
+namespace site\handler\unsubscribe;
+
+use site\Controller;
+use site\dbobject\User;
+
+class Handler extends Controller
+{
+    public function run(): void
+    {
+        $code = $this->request->get['c'];
+        if ($code) {
+            list($email, $uid) = User::decodeEmail($code);
+            $this->var['content'] = $email . ' has been unsubscribed.' . $uid;
+            return;
+        }
+        
+        $this->var['content'] = User::encodeEmail('ikki3355@gmail.com', 3);
+    }
+}
