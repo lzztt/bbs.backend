2be570e915037cb001d27a8618ab784d2ab53da3
Longzhang Tian <15257546+longztian@users.noreply.github.com>
2018-02-14 23:02:01 -0800

    new year wish emails and unsubscribe link

--- script/mail.cli.php
+++ script/mail.cli.php
@@ -1,47 +1,42 @@
 <?php declare(strict_types=1);
 
-namespace site;
+namespace script;
 
-use Exception;
+use DateTime;
 use lzx\App;
 use lzx\core\Mailer;
 use lzx\db\DB;
 use lzx\html\Template;
+use site\Config;
+use site\dbobject\User;
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
 
 // note: cache path in php and nginx are using server_name
 $_SERVER['SERVER_NAME'] = 'www.houstonbbs.com';
-
-$LZXROOT = dirname(__DIR__) . '/lib/lzx';
-
-require_once $LZXROOT . '/App.php';
 
 class MailApp extends App
 {
     public function __construct()
     {
         parent::__construct();
-        // register current namespaces
-        $this->loader->registerNamespace(__NAMESPACE__, dirname(__DIR__) . '/server');
 
         $this->timestamp = intval($_SERVER['REQUEST_TIME']);
         $this->config = Config::getInstance();
+        $this->logger->setFile($this->config->path['log'] . '/' . $this->config->domain . '.log');
+        $this->logger->setEmail($this->config->webmaster, 'Mail error', 'logger@' . $this->config->domain);
         $this->logger->addExtraInfo(['uid' => 'cron', 'umode' => 'cli', 'urole' => 'adm']);
-        $this->logger->setDir($this->config->path['log']);
-        $this->logger->setEmail($this->config->webmaster);
     }
 
-    public function run(array $args): void
+    public function run(array $args = []): void
     {
-        if (!$args) {
-            throw new Exception('need the starting user id');
-        }
-
-        $uid = $args[0];
         $db = DB::getInstance($this->config->db, true);
+        $arr = $db->query('SELECT uid FROM mails ORDER BY uid DESC limit 1');
+        $uid = $arr ? (int) array_pop(array_pop($arr)) : 0;
         $users = $db->query('SELECT id, username, email, create_time, cid FROM users WHERE id > ' . $uid . ' LIMIT 550');
-        //$users = $db->query('SELECT id, username, email, create_time, cid FROM users WHERE id > ' . $uid . ' AND email NOT LIKE "%@qq.com" LIMIT 550');
-        //$users = $db->query('SELECT id, username, email, create_time, cid FROM users WHERE id in (29634,29641,29644,29647,29675,29689,29701,29704,29707,29714,29726) or (id > 29726 and email like "%@qq.com") order by id');
-        //$users = $db->query('SELECT id, username, email, create_time, cid FROM users WHERE id > ' . $uid . ' AND email LIKE "%@qq.com" LIMIT 300');
+        // $users = $db->query('SELECT id, username, email, create_time, cid FROM users WHERE id > ' . $uid . ' AND email NOT LIKE '%@qq.com' LIMIT 550');
+        // $users = $db->query('SELECT id, username, email, create_time, cid FROM users WHERE id in (29634,29641,29644,29647,29675,29689,29701,29704,29707,29714,29726) or (id > 29726 and email like '%@qq.com') order by id');
+        // $users = $db->query('SELECT id, username, email, create_time, cid FROM users WHERE id > ' . $uid . ' AND email LIKE '%@qq.com' LIMIT 300');
         /* TEST
         $users = $db->query('SELECT id, username, email, create_time, cid FROM users WHERE id > ' . $uid . ' LIMIT 3');
         foreach ($users as $i => $u)
@@ -57,19 +52,20 @@ class MailApp extends App
         if (sizeof($users) > 0) {
             $status = [];
             Template::setLogger($this->logger);
-            Template::$path = $this->config->path['theme'] . '/' . $this->config->theme;
+            Template::$path = $this->config->path['theme'];
+            Template::$theme = $this->config->theme;
 
             foreach ($users as $i => $u) {
                 $cid = (int) $u['cid'] - 1;
                 $city = $cities[$cid];
                 $mailer = new Mailer('newyear@' . $domain[$cid] . 'bbs.com');
-                $mailer->setSubject('新年快乐，鸡年吉祥');
+                $mailer->setSubject('新年快乐，狗年吉祥');
                 $mailer->setTo($u['email']);
                 $contents = [
                     'username' => $u['username'],
-                    'time' => $this->time($u['create_time']),
+                    'time' => $this->time((int) $u['create_time']),
                     'city' => $city,
-                    'mailid' => rand()
+                    'unsubscribeLink' => 'https://www.' . $domain[$cid] . 'bbs.com/unsubscribe?c=' . User::encodeEmail($u['email'], (int) $u['id'])
                 ];
 
                 $mailer->setBody((string) new Template('mail/newyear', $contents), true);
@@ -94,40 +90,20 @@ class MailApp extends App
                 $db->query('INSERT INTO mails (uid, status) values ' . implode(',', $status));
                 $db->flush();
             }
-
-            $last = array_pop($users);
-            return $last['id'];
         }
     }
 
-    private function time($timestamp)
+    private function time(int $timestamp)
     {
         $intv = (new DateTime())->diff(new DateTime(date('Y-m-d H:i:s', $timestamp)));
+        $nums = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九', '十'];
         $days = $intv->days;
-        if ($days / 365 > 6) {
-            return '六年多以来';
-        } elseif ($days / 365 > 5) {
-            return '五年多以来';
-        } elseif ($days / 365 > 4) {
-            return '四年多以来';
-        } elseif ($days / 365 > 3) {
-            return '三年多以来';
-        } elseif ($days / 365 > 2) {
-            return '两年多以来';
-        } elseif ($days / 365 > 1) {
-            return '一年多以来';
-        } elseif ($days / 365 > 0.5) {
-            return '半年多以来';
-        } elseif ($days / 30 > 5) {
-            return '五个多月来';
-        } elseif ($days / 30 > 4) {
-            return '四个多月来';
-        } elseif ($days / 30 > 3) {
-            return '三个多月来';
-        } elseif ($days / 30 > 2) {
-            return '两个多月来';
-        } elseif ($days / 30 > 1) {
-            return '一个多月来';
+        $month = round($days / 30);
+        if ($month >= 8) {
+            $year = round($days / 365);
+            return $nums[$year] . '年来';
+        } elseif ($month > 0) {
+            return $nums[$month] . '个月来';
         }
 
         return '近期';
@@ -135,23 +111,15 @@ class MailApp extends App
 }
 
 // main program starts here
-
-$flag = '/tmp/mail/sending';
 $lock = '/tmp/mail/lock';
-if (file_exists($flag)) {
-    if (file_exists($lock)) {
-        echo 'unable to get mail sending lock, aborting';
-        exit(1);
-    } else {
-        touch($lock);
+if (file_exists($lock)) {
+    echo 'unable to get mail sending lock, aborting';
+    exit(1);
+} else {
+    touch($lock);
 
-        $uid = intval(file_get_contents($flag));
-        $app = new MailApp();
-        $uid_new = intval($app->run(1, [$uid]));
-        if ($uid_new > $uid) {
-            file_put_contents($flag, $uid_new);
-        }
+    $app = new MailApp();
+    $app->run();
 
-        unlink($lock);
-    }
+    unlink($lock);
 }
