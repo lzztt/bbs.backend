6739e7c6b8c4795b56112f6c859f1f71a48e0e8e
longztian <ikki3355@gmail.com>
2018-02-14 23:02:01 -0800

    new year wish emails and unsubscribe link

---
+++ server/theme/roselife/mail/newyear.tpl.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="zh" dir="ltr">
+  <head>
+    <meta charset="UTF-8">    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+    <article>
+      Hi <?php print $username; ?>，<br>
+      <br>
+      新的一年又到了，感谢您<?php print $time; ?>对缤纷<?php print $city; ?>网的支持和关注。<br><br>
+      祝您和您的家人 新年快乐，狗年吉祥！<br>
+      在新的一年里 身体健康，心想事成！<br>
+      <br>
+      站长<br>
+      田龙璋
+    </article>
+      <br><br><a href="<?= $unsubscribeLink ?>" style="font-size:12px;color:#999999;">退订邮件 (Unsubscribe)</a><br>
+  </body>
+</html>
