f1668f0d1846fbc08a64bfc8031723c4b7eea4aa
longztian <ikki3355@gmail.com>
2018-04-08 00:13:41 -0700

    move city info from database to config

--- server/Config.php
+++ server/Config.php
@@ -7,6 +7,25 @@ class Config
     const STAGE_DEVELOPMENT = 0;
     const STAGE_PRODUCTION = 1;
 
+    const CITIES = [
+        'houstonbbs.com' => [
+            'id' => 1,
+            'name' => '休斯顿',
+            'tidForum' => 1,
+            'tidYp' => 2,
+        ],
+        'dallasbbs.com' => [
+            'id' => 2,
+            'name' => '达拉斯',
+            'tidForum' => 127,
+        ],
+        'austinbbs.com' => [
+            'id' => 3,
+            'name' => '奥斯汀',
+            'tidForum' => 160,
+        ]
+    ];
+
     public $stage;
     public $cache;
     public $path;
@@ -17,15 +36,17 @@ class Config
     public $webmaster;
     public $image;
 
-    private function __construct(string $server_name)
+    private function __construct(string $serverName)
     {
-        $this->domain = implode('.', array_slice(explode('.', $server_name), -2));
+        $this->domain = implode('.', array_slice(explode('.', $serverName), -2));
 
         $this->stage = self::STAGE_DEVELOPMENT;
         //$this->stage = self::STAGE_PRODUCTION;
-        if (in_array($this->domain, ['houstonbbs.com', 'dallasbbs.com', 'austinbbs.com'])) {
+        if (array_key_exists($this->domain, self::CITIES)) {
             $this->stage = self::STAGE_PRODUCTION;
         }
+
+        $this->city = $this->getCity($serverName);
 
         $this->webmaster = 'mikalotus3355@gmail.com';
 
@@ -36,7 +57,7 @@ class Config
             'log' => dirname(__DIR__) . '/log',
             'file' => dirname(__DIR__) . '/client',
             'backup' => dirname(__DIR__) . '/backup',
-            'cache' => '/tmp/' . $server_name,
+            'cache' => '/tmp/' . $serverName,
         ];
         $this->cache = true;
         $this->db = [
@@ -53,6 +74,14 @@ class Config
             'width' => 600,
             'size' => 5242880,
         ];
+    }
+
+    private function getCity(string $serverName): object
+    {
+        $uriName = preg_replace(['/\w*\./', '/bbs.*/'], '', $serverName, 1);
+        $city = (object) self::CITIES[$uriName . 'bbs.com'];
+        $city->uriName = $uriName;
+        return $city;
     }
 
     public static function getInstance(): Config
