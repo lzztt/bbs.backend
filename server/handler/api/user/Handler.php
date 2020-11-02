ce9f4ed8c381c089fd21151af85b734f7da34898
Long <15257546+lzztt@users.noreply.github.com>
2020-11-01 20:11:38 -0800

    delete unused user columns

--- server/handler/api/user/Handler.php
+++ server/handler/api/user/Handler.php
@@ -23,7 +23,7 @@ class Handler extends Service
         }
 
         $uid = (int) $this->args[0];
-        $user = new User($uid, 'username,wechat,qq,website,sex,birthday,relationship,occupation,interests,favoriteQuotation,createTime,lastAccessTime,lastAccessIp,avatar,points,status');
+        $user = new User($uid, 'username,favoriteQuotation,createTime,lastAccessTime,lastAccessIp,avatar,points,status');
 
         if ($user->status > 0) {
             $info = $user->toArray();
