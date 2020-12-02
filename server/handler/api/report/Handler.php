9aa7751c5499338988c29b456ad837a42a21fc71
Long <15257546+lzztt@users.noreply.github.com>
2020-12-01 23:05:46 -0800

    add reporter not found error and user link for spammer

--- server/handler/api/report/Handler.php
+++ server/handler/api/report/Handler.php
@@ -107,6 +107,7 @@ class Handler extends Service
             foreach (array_merge(...array_values($complainGroups)) as $uid) {
                 $reporter = new User($uid, 'contribution,status');
                 if (!$reporter->exists() || $reporter->status !== 1) {
+                    $this->logger->error('reporter user not found: ' . $uid);
                     continue;
                 }
                 $reporter->contribution += 1;
@@ -120,7 +121,7 @@ class Handler extends Service
 
             $this->postTopic(
                 $spammer->username . '被系统封禁' . $days . '天',
-                '原因：' . $spammer->username . '在[url=/node/' . $comment->nid . ']这个帖子[/url]里的发言被多位用户举报。' . PHP_EOL
+                '原因：[url=/user/' . $spammer->id . ']' . $spammer->username . '[/url]在[url=/node/' . $comment->nid . ']这个帖子[/url]里的发言被多位用户举报。' . PHP_EOL
                     . '类别：'  . $reason . PHP_EOL
                     . '惩罚：用户被封禁' . $days . '天，用户的声望和贡献各减3点。'
             );
