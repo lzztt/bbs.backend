843163b3612c9c68a4afc7147d568ee6787e5feb
Long <15257546+lzztt@users.noreply.github.com>
2020-12-07 23:09:19 -0800

    add pm_icon class to hide icons

--- server/theme/roselife/author_panel_forum.tpl.php
+++ server/theme/roselife/author_panel_forum.tpl.php
@@ -14,7 +14,7 @@ function (
         <?= mb_substr($username, 0, (preg_match('/^[A-Za-z0-9]{3}/', $username) ? 3 : 2)) ?>
       </div>
     <?php endif ?>
-    <div>
+    <div class="pm_icon">
       <svg class="MuiSvgIcon-root" focusable="false" viewBox="0 0 24 24" aria-hidden="true" style="color:#3f51b5; cursor:pointer;" onclick='window.app.openMsgEditor({toUser: {id:<?= $uid ?>,username:"<?= $username ?>"}})'>
         <path d="M20 4H4c-1.1 0-1.99.9-1.99 2L2 18c0 1.1.9 2 2 2h16c1.1 0 2-.9 2-2V6c0-1.1-.9-2-2-2zm0 14H4V8l8 5 8-5v10zm-8-7L4 6h16l-8 5z"></path>
       </svg>
