0c53cb16fcd750869f74ec34ed3afe7b9e78ee0a
Long <15257546+lzztt@users.noreply.github.com>
2023-02-10 23:25:36 -0800

    Remove alexa stats

--- server/handler/api/stat/Handler.php
+++ server/handler/api/stat/Handler.php
@@ -17,19 +17,6 @@ class Handler extends Service
         $node = new Node();
         $r = $node->getNodeStat(self::$city->tidForum);
 
-        $alexaCache = $this->getIndependentCache('alexa');
-        $alexa = $alexaCache->getData();
-        if (!$alexa) {
-            $alexa = $this->getAlexa(self::$city->domain);
-            if ($alexa) {
-                $alexaCache->setData(Template::fromStr($alexa));
-            }
-        } else {
-            $alexa = (string) $alexa;
-        }
-
-        $r['alexa'] = $alexa;
-
         $user = new User();
         $u = $user->getUserStat($this->request->timestamp - 300, self::$city->id);
 
@@ -58,27 +45,5 @@ class Handler extends Service
             $u['onlineGuestCount'] = $u['onlineCount'] - $u['onlineUserCount'];
         }
         $this->json(array_merge($r, $u));
-    }
-
-    private function getAlexa(string $domain): string
-    {
-        $data = self::curlGet('http://data.alexa.com/data?cli=10&dat=s&url=' . $domain);
-
-        if ($data) {
-            preg_match('#<POPULARITY URL="(.*?)" TEXT="([0-9]+){1,}"#si', $data, $p);
-            if (count($p) > 2 && $p[2]) {
-                $rank = number_format(intval($p[2]));
-                return $this->getSiteName()
-                    . '最近三个月平均访问量<a target="_blank" href="https://www.alexa.com/siteinfo/'
-                    . $domain
-                    . '">Alexa排名</a>:<br><a target="_blank" href="https://www.alexa.com/siteinfo/'
-                    . $domain . '">第 <b>'
-                    . $rank . '</b> 位</a> (更新时间: ' . date('m/d/Y H:i:s T', $this->request->timestamp)
-                    . ')';
-            }
-        }
-
-        $this->logger->warning('Get Alexa Rank Error');
-        return '';
     }
 }
